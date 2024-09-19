7d31a340f76b5b9ba135abdce08251988e40e026
Anzinius <67458912+Anzinius@users.noreply.github.com>
2024-09-19 23:11:20 +0900

    Make code a simple class

--- 02.splash_screen/splash_screen/splash_screen/lib/main.dart
+++ 02.splash_screen/splash_screen/splash_screen/lib/main.dart
@@ -5,20 +5,27 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFF3256a8),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'asset/img/logo.png',
-            ),
-            CircularProgressIndicator(
-              color: Colors.white,
-            ),
-          ],
-        ),
-      ),
+      home: HomeScreen(),
     ),
   );
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF3256a8),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'asset/img/logo.png',
+          ),
+          CircularProgressIndicator(
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
 }
