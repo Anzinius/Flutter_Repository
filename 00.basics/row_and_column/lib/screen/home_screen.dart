a7810fd49e19b232a14dd48ff43967503307a1c5
Anzinius <67458912+Anzinius@users.noreply.github.com>
2024-10-01 21:57:03 +0900

    Make a answer of Challenge 1

--- 00.basics/row_and_column/lib/screen/home_screen.dart
+++ 00.basics/row_and_column/lib/screen/home_screen.dart
@@ -11,20 +11,120 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          color: Colors.black,
+          color: Colors.white,
+          height: double.infinity,
           child: Column(
-            children: colors
-                .map(
-                  (e) => Container(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
                     height: 50.0,
                     width: 50.0,
-                    color: e,
+                    color: Colors.red,
                   ),
-                )
-                .toList(),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.orange,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                //mainAxisSize: MainAxisSize.min,
+                //crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                //mainAxisSize: MainAxisSize.min,
+                //crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.orange,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                //mainAxisSize: MainAxisSize.min,
+                //crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    height: 50.0,
+                    width: 50.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+
+//  [
+//             Flexible(
+//               flex: 2,
+//               fit: FlexFit.loose,
+//               child: Container(
+//                 height: 50.0,
+//                 width: 50.0,
+//                 color: Colors.red,
+//               ),
+//             ),
+//             Expanded(
+//               flex: 3,
+//               child: Container(
+//                 height: 50.0,
+//                 width: 50.0,
+//                 color: Colors.orange,
+//               ),
+//             ),
+//             Expanded(
+//               child: Container(
+//                 height: 50.0,
+//                 width: 50.0,
+//                 color: Colors.yellow,
+//               ),
+//             ),
+//           ]
+          
